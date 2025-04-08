1475b4a3195a7dcace540c420aee6c371d17866c
MotiWolff <motiwolff@gmail.com>
2025-04-08 16:46:02 +0300

    Forcefully remove service worker and prevent caching

--- unregister-sw.js
+++ unregister-sw.js
@@ -1,8 +1,24 @@
+// Force unregister all service workers and clear all caches
 if ('serviceWorker' in navigator) {
+    // Unregister all service workers
     navigator.serviceWorker.getRegistrations().then(function(registrations) {
         for(let registration of registrations) {
             registration.unregister();
-            console.log('Service worker unregistered:', registration);
         }
     });
+
+    // Force the service worker to skip waiting and activate immediately
+    navigator.serviceWorker.ready.then(registration => {
+        registration.active.postMessage({type: 'SKIP_WAITING'});
+    });
+
+    // Clear all caches
+    caches.keys().then(function(names) {
+        for (let name of names) {
+            caches.delete(name);
+        }
+    });
+
+    // Reload the page after cleanup
+    window.location.reload(true);
 } 
